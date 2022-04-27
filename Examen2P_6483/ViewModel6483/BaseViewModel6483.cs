e0b0aebfb6621e054854e72ca7362eb87b25ff1a
Josue Iglesias <jiglesias3@ucol.mx>
2022-04-27 12:51:57 -0500

    BaseViewModel añadido

---
+++ Examen2P_6483/ViewModel6483/BaseViewModel6483.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Examen2P_6483.ViewModel6483
+{
+    internal class BaseViewModel6483 : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
