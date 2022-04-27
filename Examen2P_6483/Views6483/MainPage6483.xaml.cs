9d23e1b940c73d6805410f40eb62e7964cb7c9af
Josue Iglesias <jiglesias3@ucol.mx>
2022-04-27 12:29:37 -0500

    Primer Commit

---
+++ Examen2P_6483/Views6483/MainPage6483.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen2P_6483.Views6483
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage6483 : ContentPage
+    {
+        public MainPage6483()
+        {
+            InitializeComponent();
+        }
+    }
+}
