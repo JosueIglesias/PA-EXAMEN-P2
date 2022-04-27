561f2dc42776c234e5f906dfb362d5ccb23a09d8
Josue Iglesias <jiglesias3@ucol.mx>
2022-04-27 12:45:59 -0500

    Clases de user y account añadidas

---
+++ Examen2P_6483/Models6483/Account6483.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen2P_6483.Models6483
+{
+    public class Account6483
+    {
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public int Balance { get; set; }
+    }
+}
---
+++ Examen2P_6483/Models6483/User6483.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen2P_6483.Models6483
+{
+    public class User6483
+    {
+        public string FirstName { get; set; }
+        public string PaternalLastName { get; set; }
+        public string MaternalLastName { get; set; }
+        public int Phone { get; set; }
+    }
+}
